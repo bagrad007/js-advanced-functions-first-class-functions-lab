7f971c27be019359d4f0dd4365263410aa8f6e3f
Luke Gill <bagrad007@gmail.com>
2021-04-23 13:07:52 -0400

    commit tree 54590faf123a64344451a5bd2c27897ac2652774

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(array) {
+    return array.slice(0, 2)
+}
+
+function returnLastTwoDrivers(array) {
+    return array.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+        return multiplyValue * value;
+    };
+};
+
+
+let fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (array, drivers) {
+    return drivers(array)
+}
